27e39cc01589451868ae97232889bfee4740ca4f
AURORA_NT\jack.kao <jack.kao@rovisys.com>
2023-06-17 12:03:02 +0800

    Before changing navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { Hero } from './hero';
+import { Component, inject } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
+
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Mars',
-  };
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  // 第一種可以透過Service取值的方法
+  // heroService: HeroService = inject(HeroService);
+  // constructor() {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  // 第二種透過Service取值的方法，官方建議
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
